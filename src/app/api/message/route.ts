173fb24304b9bdc0180c51ef0926b9e4ef4e334b
Vivek Hipparkar <vivekhipparkar@gmail.com>
2024-08-20 22:01:04 +0530

    user message model and route added

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    //endpoint for asking questions to the pdf
+    const body = req.json();
+
+    const { getUser } = getKindeServerSession()
+    const user = await getUser();
+
+    const userId = user?.id;
+
+    if (!userId) return new Response("Unauthorized", { status: 401 })
+
+    const { fileId, message } = SendMessageValidator.parse(body)
+
+    const file = await db.file.findFirst({
+        where: {
+            id: fileId,
+            userId
+        }
+    })
+
+    if (!file) return new Response("File not found", { status: 404 })
+
+    await db.message.create({
+        data:{
+            text:message,
+            isUserMessage:true,
+            userId,
+            fileId
+        }
+    })
+
+    //
+}
